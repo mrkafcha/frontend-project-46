911e77dea10c4ec13c5a7d50af7d1bf2b813a152
Aleksey Chiglintsev <chiglincev2010@bk.ru>
2024-01-29 15:36:49 +0500

    update content gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,12 @@
+#!/usr/bin/env node
 import { program } from 'commander';
 
-programm
+program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
+program.parse();
