af73779377a41e8b9d96685671fb99b8d5b8bf16
Aleksey Chiglintsev <chiglincev2010@bk.ru>
2024-04-12 14:04:33 +0500

    Update content stylish.js

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
 
 const getIndents = (depth) => {
-  const indent = '';
-  const indentSize = depth * 4;
-  return indent.repeat(indentSize - 2);
+  const indent = '  ';
+  const indentSize = depth * 2;
+  return {
+    leftIndent: indent.repeat(indentSize - 1),
+    rightIndent: indent.repeat(indentSize - 2),
+  }
+  ;
 };
 
 const renderToString = (value, depth) => {
@@ -11,32 +15,39 @@ const renderToString = (value, depth) => {
     return String(value);
   }
   const idents = getIndents(depth);
-  const lines = Object.entries(value)
-    .map(([key, val]) => `${idents}  ${key}: ${renderToString(val, depth + 1)}`);
-  return [
-    '{',
-    ...lines,
-    `${idents}}`,
-  ].join('\n');
+
+//   const lines = Object.entries(value)
+//     .map(([key, val]) => `${idents.leftIndent}  ${key}: ${renderToString(val, depth + 1)}`);
+  const lines = Object.entries(value).map(([key, val]) => {
+    if (!_.isObject(val)) {
+      return `${idents.leftIndent}  ${key}: ${val}`;
+    }
+
+    return `${idents.leftIndent}  ${key}: ${renderToString(val, depth + 1)}`;
+  });
+  // return ['{', ...lines, `${idents.rightIndent}}`].join('\n');
+  return `{\n${lines.join('\n')}\n${idents.rightIndent}}`;
 };
 
 export default (tree) => {
+  
   const iter = (node, depth) => {
+    const idents = getIndents(depth);
     const result = node.map(({
       key, value, type, newValue,
     }) => {
-      const idents = getIndents(depth);
+      const renderValue = renderToString(value, depth + 1);
       switch (type) {
         case 'unchanged':
-          return `${idents}  ${key}: ${(value)}`;
+          return `${idents.leftIndent}  ${key}: ${renderValue}`;
         case 'changed':
-          return `${idents}- ${key}: ${renderToString(value, depth)}${idents}\n+ ${key}: ${renderToString(newValue, depth)}`;
+          return `${idents.leftIndent}- ${key}: ${renderValue}\n${idents.leftIndent}+ ${key}: ${renderToString(newValue, depth + 1)}`;
         case 'deleted':
-          return `${idents}- ${key}: ${renderToString(value, depth)}`;
+          return `${idents.leftIndent}- ${key}: ${renderValue}`;
         case 'added':
-          return `${idents}+ ${key}: ${renderToString(value, depth)}`;
+          return `${idents.leftIndent}+ ${key}: ${renderValue}`;
         case 'nested':
-          return `${idents}  ${key}: {\n${iter(value, depth + 1)}\n}`;
+          return `${idents.leftIndent}  ${key}: {\n${iter(value, depth + 1)}\n${idents.rightIndent}}`;
         default:
           throw new Error('This type is not in use.');
       }
