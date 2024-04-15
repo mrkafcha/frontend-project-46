ff2d7cc2ec8d7bff52920c7ea15779bbaf20c4eb
Aleksey Chiglintsev <chiglincev2010@bk.ru>
2024-04-15 16:33:27 +0500

    Update content idex.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import { cwd } from 'node:process';
 import { resolve, extname } from 'node:path';
 import fs from 'node:fs';
-import сomparisonFile from './getTree.js';
-import formatter from './formaters/index.js';
-import parsers from './parsers.js';
+import getTree from './getTree.js';
+import getFormatDiff from './formaters/index.js';
+import getParseFile from './parsers.js';
 
 const readFile = (filepath) => fs.readFileSync(resolve(cwd(), filepath), 'utf-8');
 const getFormat = (filepath) => extname(filepath);
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const file1 = parsers(readFile(filepath1), getFormat(filepath1));
-  const file2 = parsers(readFile(filepath2), getFormat(filepath2));
-  const getDiffTree = сomparisonFile(file1, file2);
-  const result = formatter(getDiffTree, format);
+  const file1 = getParseFile(readFile(filepath1), getFormat(filepath1));
+  const file2 = getParseFile(readFile(filepath2), getFormat(filepath2));
+  const getDiffTree = getTree(file1, file2);
+  const result = getFormatDiff(getDiffTree, format);
 
   return result;
 };
